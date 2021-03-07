72388f1e11425340e12f1e8a6ac24fc2d101fe2f
CherryLU1993 <32975719+CherryLU1993@users.noreply.github.com>
2021-03-07 18:41:07 +0900

    Task 2 update and delete notifications.

--- app/src/main/java/com/rikucherry/notifyme/MainActivity.java
+++ app/src/main/java/com/rikucherry/notifyme/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,6 +18,9 @@ import androidx.core.app.NotificationCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonNotify;
+    private Button buttonUpdate;
+    private Button buttonCancel;
+
     // Both channel and notification need id
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private static final int NOTIFICATION_ID = 1234;
@@ -29,11 +34,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonNotify = findViewById(R.id.notify);
+        buttonUpdate = findViewById(R.id.update);
+        buttonCancel = findViewById(R.id.cancel);
+
+        setNotificationButtonState(true,false,false);
 
         createNotificationChannel();
-        buttonNotify.setOnClickListener(view -> sendNotification());
-    }
+        buttonNotify.setOnClickListener(view -> {
+            sendNotification();
+            setNotificationButtonState(false,true,true);
+        });
 
+        buttonUpdate.setOnClickListener(view -> {
+            updateNotification();
+            setNotificationButtonState(false,false,true);
+        });
+        buttonCancel.setOnClickListener(view -> {
+            cancelNotification();
+            setNotificationButtonState(true,false,false);
+        });
+
+    }
 
     public void createNotificationChannel() {
         mNotifyManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
@@ -52,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Notification  getNotification(){
+    private NotificationCompat.Builder  getNotificationBuilder(){
 
         Intent notificationIntent = new Intent(this,MainActivity.class);
         // note：通知的content intent必须被pending intent封装
@@ -61,23 +82,46 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
                 NOTIFICATION_ID,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Notification notification = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                 .setContentTitle("收到一则提醒！")
                 .setContentText("开饭了！")
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true) // 用户点击后自动关闭
-                .setPriority(NotificationCompat.PRIORITY_HIGH) // for API < 28
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .build();
+                .setAutoCancel(true); // 用户点击后自动关闭
 
-        return notification;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+                builder.setPriority(NotificationCompat.PRIORITY_HIGH) // for API < 28
+                .setDefaults(NotificationCompat.DEFAULT_ALL);}
+
+        return builder;
     }
 
     public void sendNotification() {
-        Notification customisedNotification = getNotification();
+        Notification customisedNotification = getNotificationBuilder().build();
         mNotifyManager.notify(NOTIFICATION_ID,customisedNotification);
     }
 
+    private void updateNotification() {
+        Bitmap notificationImage = BitmapFactory.decodeResource(getResources(), R.drawable.mascot_1);
+        Notification updatedNotification = getNotificationBuilder()
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(notificationImage)
+                .setBigContentTitle("已更新您的通知！"))
+                .build();
+
+        mNotifyManager.notify(NOTIFICATION_ID,updatedNotification);
+    }
+
+    private void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void setNotificationButtonState (Boolean isNotifyEnabled,
+                                             Boolean isUpdateEnabled,
+                                             Boolean isCancelEnabled) {
+        buttonNotify.setEnabled(isNotifyEnabled);
+        buttonUpdate.setEnabled(isUpdateEnabled);
+        buttonCancel.setEnabled(isCancelEnabled);
+    }
 
 }
